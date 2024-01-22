e342280e7543c4d9371792f273fc6946b7578547
yuzen9622 <125567280+yuzen9622@users.noreply.github.com>
2024-01-22 21:49:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './nav/navbar';
-import {HashRouter as Route, Routes, Navigate } from 'react-router-dom';
+import {HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Dash from './dash/Dash';
 import Acc from './account/Acc';
 import { useEffect, useState } from 'react';
@@ -20,7 +20,9 @@ function App() {
   })
 
   return (
+   
     <div className="App">
+       <HashRouter>
       <Navbar />
       <Routes basename={ process.env.Account-App }>
         <Route  path={'/'} element={user ? <Navigate to='/dash/' /> : <Navigate to='/account' />} />
@@ -30,6 +32,7 @@ function App() {
         <Route path='/setting/*' element={<Console />} />
 
       </Routes>
+       </HashRouter>
     </div>
 
   );
