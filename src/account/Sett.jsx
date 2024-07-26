7a2e4df84f0c1ce36e24200d032d6db39dbc5d44
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-07-26 09:29:27 +0800

    update rwd 新增日期選項

--- src/account/Sett.jsx
+++ src/account/Sett.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./sett.css";
 import { useState, useEffect } from "react";
 import { url } from "../service";
@@ -10,30 +10,41 @@ function Sett() {
   const [source, setsource] = useState("");
   const [intotal, setinTotal] = useState(0);
   const [outtotal, outsetTotal] = useState(0);
-  const [type, settype] = useState();
+  const [type, settype] = useState("");
   const [mtype, setmtype] = useState();
   const [error, seterror] = useState("");
+  const [month, setMonth] = useState(null);
+
+  const [loading, setLoading] = useState(false);
 
   const gettype = (select) => {
     setIntype([]);
     setOuttype([]);
+    setLoading(true);
+    if (!select) return;
     let acctip = document.getElementById("account-tip");
     let acctable = document.getElementById("account-table");
     let acctype = document.getElementById("account-type");
-    fetch(`${url}/account_api/getType.php?name=${user}&selects=${select}`)
+    fetch(
+      `${url}/account_api/getType.php?name=${user}&selects=${select}&month=${month}`
+    )
       .then((res) => res.json())
       .then((data) => {
         var in_datas = [];
         var out_datas = [];
-
+        if (data.ok === "false") {
+          return;
+        }
         for (let i = 0; i < data.response.length; i++) {
-          if (data.response[i].source == "in") {
+          if (data.response[i].source === "in") {
             in_datas.push({
+              source: data.response[i].source,
               mtype: data.response[i].mtype,
               price: data.response[i].price,
             });
-          } else if (data.response[i].source == "out") {
+          } else if (data.response[i].source === "out") {
             out_datas.push({
+              source: data.response[i].source,
               mtype: data.response[i].mtype,
               price: data.response[i].price,
             });
@@ -42,23 +53,27 @@ function Sett() {
         setIntype(in_datas);
         setOuttype(out_datas);
       });
+    setLoading(false);
     settype(select);
     acctip.style.display = "none";
     acctable.style.display = "flex";
     acctype.style.display = "none";
   };
 
-  const getidtype = (select) => {
+  const getidtype = (select, sources) => {
     setmtype([]);
     setIntype([]);
     let acctip = document.getElementById("account-tip");
     let acctable = document.getElementById("account-table");
     let acctype = document.getElementById("account-type");
     fetch(
-      `${url}/account_api/getIDtype.php?name=${user}&selects=${select}&mtype=${type}`
+      `${url}/account_api/getIDtype.php?name=${user}&selects=${select}&mtype=${type}&month=${month}&source=${sources}`
     )
       .then((res) => res.json())
       .then((data) => {
+        if (data.ok === "false") {
+          return;
+        }
         var datas = [];
         for (let i = 0; i < data.response.length; i++) {
           var inputDateStr = data.response[i].time;
@@ -103,6 +118,15 @@ function Sett() {
   };
 
   useEffect(() => {
+    gettype(type);
+  }, [month]);
+  useEffect(() => {
+    let date = new Date();
+    let formatMonth =
+      date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    let full = `${date.getFullYear()}-${formatMonth}`;
+
+    setMonth(full);
     var inid = `${url}/account_api/getId.php?name=${user}&selects=mtype`;
     fetch(inid)
       .then((res) => res.json())
@@ -110,7 +134,7 @@ function Sett() {
         var datas = [];
         let in_total = 0;
         let out_total = 0;
-        if (data.ok == "true") {
+        if (data.ok === "true") {
           for (let i = 0; i < data.response.length; i++) {
             in_total += parseInt(data.response[i].total_in);
             out_total += parseInt(data.response[i].total_out);
@@ -135,7 +159,7 @@ function Sett() {
         <h1>帳戶</h1>
         <hr />
       </div>
-      {error == "" ? (
+      {error === "" ? (
         <div className="user-sett">
           <div className="total">
             <div className="all-price">
@@ -146,7 +170,13 @@ function Sett() {
             </div>
             <div className="ac">
               <div className="box">
-                <h3 style={{ color: "rgb(56, 205, 9)" }}>
+                <h3
+                  style={
+                    intotal - outtotal >= 0
+                      ? { color: "rgb(56, 205, 9)" }
+                      : { color: "rgb(241, 38, 11)" }
+                  }
+                >
                   {intotal - outtotal}
                 </h3>
                 <p>總資產</p>
@@ -168,7 +198,7 @@ function Sett() {
               <div className="account-btn">
                 <button
                   onClick={() => {
-                    gettype(`${datas.mtype}`);
+                    gettype(datas.mtype);
                   }}
                 >
                   <h4>{datas.mtype}</h4>{" "}
@@ -182,20 +212,26 @@ function Sett() {
 
           <div className="account-table" id="account-table">
             <h2>{type}</h2>
+            <input
+              type="month"
+              onChange={(e) => setMonth(e.target.value)}
+              value={month}
+              id="time"
+            />
             {intype.length > 0 ? (
               <>
                 <div className="top">
                   <button onClick={closetype}>
                     <i class="fa-solid fa-chevron-left"></i>
                   </button>
-                  <h3>1月份收入類別</h3>
+                  <h3>月收入類別</h3>
                 </div>
                 {intype.map((data, key) => (
                   <div className="data">
                     <div className="data-inout account-btn" key={key}>
                       <button
                         onClick={() => {
-                          getidtype(`${data.mtype}`);
+                          getidtype(`${data.mtype}`, `${data.source}`);
                         }}
                       >
                         <h5>{data.mtype} </h5>{" "}
@@ -217,14 +253,14 @@ function Sett() {
             {Outtype.length > 0 || intype.length > 0 ? (
               <>
                 <div className="top">
-                  <h3>1月份支出類別</h3>
+                  <h3>月支出類別</h3>
                 </div>
                 {Outtype.map((data, key) => (
                   <div className="data">
                     <div className="data-inout account-btn" key={key}>
                       <button
                         onClick={() => {
-                          getidtype(`${data.mtype}`);
+                          getidtype(`${data.mtype}`, `${data.source}`);
                         }}
                       >
                         <h5>{data.mtype} </h5>{" "}
@@ -237,13 +273,17 @@ function Sett() {
                   </div>
                 ))}
               </>
-            ) : (
+            ) : loading ? (
               <h1>Loading...</h1>
+            ) : (
+              <>
+                <h3>暫無資料</h3>
+              </>
             )}
           </div>
 
           <div id="account-type" className="account-table">
-            {source == "in" ? (
+            {source === "in" ? (
               <>
                 <div className="top">
                   <button onClick={closetype}>
@@ -251,7 +291,7 @@ function Sett() {
                   </button>
                   <h3>{mtype}收入類</h3>
                 </div>
-                {intype.length == 0 ? (
+                {intype.length === 0 ? (
                   <h1>Loading...</h1>
                 ) : (
                   intype.map((data, key) => (
@@ -273,9 +313,9 @@ function Sett() {
                   <button onClick={closetype}>
                     <i class="fa-solid fa-chevron-left"></i>
                   </button>
-                  <h3>{mtype}收入類</h3>
+                  <h3>{mtype}支出類</h3>
                 </div>
-                {intype == 0 ? (
+                {intype === 0 ? (
                   <h1>Loading...</h1>
                 ) : (
                   intype.map((data, key) => (
