cbd7465a22c0fd67f1b3d8869fe2ebed5913886b
yuzen9622 <oscar48079@gmail.com>
2025-02-15 11:17:52 +0800

    change char lower

--- src/setting/Console.jsx
+++ src/setting/Console.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./console.css";
 import Finances from "./categoryType";
-import Navsett from "./navSett";
+import Navsett from "./navsett";
 import AccountType from "./accountType";
 import { Route, Routes } from "react-router-dom";
 import UserSetting from "./UserSetting";
